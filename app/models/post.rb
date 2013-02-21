c28ded2ea9f86a267cc2a3aed4a5c7fc6b596064
Brock Trappitt <brock.trappitt@gmail.com>
2013-02-21 21:08:47 +0800

    Latest versions

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
-  include Extras
   include Constants
+  include Extras
   attr_accessible :body, :published, :title
   new_class_method :bar
   declare_constants something: 1, else: 2
