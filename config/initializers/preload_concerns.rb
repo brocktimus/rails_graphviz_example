c35b18d1899e2ad81ed2e64d66eb5a7a35c64a8f
Brock Trappitt <brock.trappitt@gmail.com>
2013-02-21 21:21:17 +0800

    Fixed broken reason

--- config/initializers/preload_concerns.rb
+++ config/initializers/preload_concerns.rb
@@ -1,6 +1,3 @@
-if Rails.env.development?
-  Dir.glob(Rails.root.join('app', 'models', 'concerns', '*.rb').to_s).each do |file|
-    require_dependency file
-  end
+Dir.glob(Rails.root.join('app', 'models', 'concerns', '*.rb').to_s).each do |file|
+  require_dependency file
 end
-
